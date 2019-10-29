329dbfffc230219f3a63ec27e364738aa7aa4d0e
Lilibeth Santo <itzlilibeth@gmail.com>
2019-10-29 19:50:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,7 @@
 class Person
-  PERSON 1 = adele_goldberg
-  PERSON 2 =alan_kay
+  adele_goldberg = person.new
+  alan_kay
 end  
 
-  fido = Person.new
-  snoopy = Person.new
-  lassie = Person.newelarn
+  
   
